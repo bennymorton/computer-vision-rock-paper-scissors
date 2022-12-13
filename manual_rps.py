0621aae419c01a1a83b60bc5016e104f87d3fa91
Ben <benjaminmorton@live.co.uk>
2022-12-13 14:44:14 +0000

    Game logic wrapped inside function

--- manual_rps.py
+++ manual_rps.py
@@ -1,36 +1,36 @@
 import random
 
 # def play():
-    def get_computer_choice():
-        options = ['Rock', 'Paper', 'Scissors']
-        return random.choice(options)
+def get_computer_choice():
+    options = ['Rock', 'Paper', 'Scissors']
+    return random.choice(options)
 
-    def get_user_choice():
-        user_choice = input('Rock, paper, scissors, go!')
-        return user_choice
+def get_user_choice():
+    user_choice = input('Rock, paper, scissors, go!')
+    return user_choice
 
-    def get_winner(computer_choice, user_choice):
-        # user winning outcomes
-        if computer_choice == "Rock" and user_choice == "Paper":
-            print("You won!")
-        elif computer_choice == "Scissors" and user_choice == "Rock":
-            print("You won!")
-        elif computer_choice == "Paper" and user_choice == "Scissors":
-            print("You won!")
-        # computer winning scenarios
-        elif computer_choice == "Rock" and user_choice == "Scissors":
-            print("You lost")
-        elif computer_choice == "Scissors" and user_choice == "Paper":
-            print("You lost")
-        elif computer_choice == "Paper" and user_choice == "Rock":
-            print("You lost")
-        # tieing scenarios
-        else:
-            print("It is a tie!")
+def get_winner(computer_choice, user_choice):
+    # user winning outcomes
+    if computer_choice == "Rock" and user_choice == "Paper":
+        print("You won!")
+    elif computer_choice == "Scissors" and user_choice == "Rock":
+        print("You won!")
+    elif computer_choice == "Paper" and user_choice == "Scissors":
+        print("You won!")
+    # computer winning scenarios
+    elif computer_choice == "Rock" and user_choice == "Scissors":
+        print("You lost")
+    elif computer_choice == "Scissors" and user_choice == "Paper":
+        print("You lost")
+    elif computer_choice == "Paper" and user_choice == "Rock":
+        print("You lost")
+    # tieing scenarios
+    else:
+        print("It is a tie!")
 
-    computer_choice = get_computer_choice()
-    user_choice = get_user_choice()
+computer_choice = get_computer_choice()
+user_choice = get_user_choice()
 
-    get_winner(computer_choice, user_choice)
+get_winner(computer_choice, user_choice)
 
 # play()
